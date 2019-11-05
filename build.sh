d26d7f8c0f8c41de1be1be1be329c769e308f42f
RedEnemy30 <nztmscreempendi@yahoo.co.id>
2019-11-05 07:37:14 +0000

    fix

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=NzTmPendi
-export KBUILD_BUILD_HOST=Roar
-CROSS_COMPILE="/projects/prebuilts_gcc_linux-x86_arm-linux-androideabi-4.9/bin/arm-linux-androideabi-"
+export KBUILD_BUILD_HOST=Server
+CROSS_COMPILE="/projects/tc/bin/arm-linux-androideabi-"
 export CROSS_COMPILE
 make O=out cactus_defconfig
 make O=out
